51b5c01181582c6d88bc97d787dd006b1fefdf8d
sash <softester@yandex.ru>
2022-12-15 05:07:49 +0300

    Get price working!

--- src/main/java/at/example/Main.java
+++ src/main/java/at/example/Main.java
@@ -13,31 +13,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            Document document = documentBuilder.parse("BookCatalog.xml");
-            Node root = document.getDocumentElement();
-            System.out.println("List of books:");
-            System.out.println();
-            NodeList books = root.getChildNodes();
-            for (int i = 0; i < books.getLength(); i++) {
-                Node book = books.item(i);
-                if (book.getNodeType() != Node.TEXT_NODE) {
-                    NodeList bookProps = book.getChildNodes();
-                    for (int j = 0; j < bookProps.getLength(); j++) {
-                        Node bookProp = bookProps.item(j);
-                        if (bookProp.getNodeType() != Node.TEXT_NODE) {
-                            System.out.println(bookProp.getNodeName() + ":" + bookProp.getChildNodes().item(0).getTextContent());
-                        }
-                    }
-                }
-            }
-        } catch (ParserConfigurationException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (SAXException e) {
-            throw new RuntimeException(e);
-        }
+        System.out.println("Main.example");
     }
 }
--- src/main/java/at/neuro/Neuro.java
+++ src/main/java/at/neuro/Neuro.java
@@ -1,7 +1,18 @@
 package at.neuro;
 
+import com.webcerebrium.binance.api.BinanceApi;
+import com.webcerebrium.binance.api.BinanceApiException;
+
+import java.math.BigDecimal;
+
 public class Neuro {
     public static void main (String[] args) {
-        System.out.println("Neuro is working!)" + args[0] + args[1]);
+        BinanceApi api = new BinanceApi();
+        try {
+            BigDecimal asset = api.pricesMap().get("TRXUSDT");
+            System.out.println(asset);
+        } catch (BinanceApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
