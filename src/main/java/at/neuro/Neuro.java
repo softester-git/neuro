a89931c54ca830f79c1ef7f485b2674b55ae9454
sash <softester@yandex.ru>
2023-01-05 11:21:25 +0300

    Plot added 2

--- src/main/java/at/neuro/Neuro.java
+++ src/main/java/at/neuro/Neuro.java
@@ -1,22 +1,51 @@
 package at.neuro;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
+import netscape.javascript.JSObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static at.neuro.TestFramework.testWork;
 import static at.neuro.constant.Urls.URLDATA;
 
 public class Neuro {
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
-//        Klines.symbol = "TRXUSDT";
-//        Klines.interval = "1h";
-//        Klines.limit = 100;
-//        Klines.klinesInit();
+        Klines.symbol = "TRXUSDT";
+        Klines.interval = "1h";
+        Klines.limit = 100;
+        Klines.klinesInit();
 
-//        System.out.println(ApiClient.statusCode);
-//        System.out.println(ApiClient.body);
+        JSONArray ja = new JSONArray(ApiClient.body);
+        double[][] val0 = new double[ja.length()][2];
+        String[][] val1 = new String[ja.length()][2];
+        double max0 = 0;
+        double min0 = 1000000;
 
-        Plot.x = new double[]{3.3,4.8,5.5,4.6,3.3,5.4};
-        Plot.y = new double[]{2.1,1.7,2.4,3.6,4.3,5.2};
+        for (int i=0; i<ja.length(); i++) {
+            JSONArray jElem = new JSONArray(ja.get(i).toString());
+
+            val0[i][0] = Double.parseDouble(String.valueOf(i));
+
+            val0[i][1] = Double.parseDouble(jElem.get(1).toString());
+            if (val0[i][1] > max0) max0 = val0[i][1];
+            if (val0[i][1] < min0) min0 = val0[i][1];
+        }
+        Plot.values0 = val0;
+        Plot.max0 = max0;
+        Plot.min0 = min0;
+        Plot.legend = Klines.symbol + " " + Klines.interval;
         Plot.plotInit();
+
+        //testWork();
     }
 }
--- src/main/java/at/neuro/Plot.java
+++ src/main/java/at/neuro/Plot.java
@@ -1,28 +1,76 @@
 package at.neuro;
 
-import org.math.plot.*;
+import com.panayotis.gnuplot.JavaPlot;
+import com.panayotis.gnuplot.plot.DataSetPlot;
+import com.panayotis.gnuplot.style.NamedPlotColor;
+import com.panayotis.gnuplot.style.PlotStyle;
+import com.panayotis.gnuplot.style.Style;
+import com.panayotis.gnuplot.terminal.ImageTerminal;
 
-import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class Plot {
-    public static double[] x;
-    public static double[] y;
+    public static double[][] values0;
+    public static double max0;
+    public static double min0;
+    public static String[][] values1;
+    public static double[][] values2;
+    public static double[][] values3;
+    public static String legend;
 
     public static void plotInit() {
+        ImageTerminal png = new ImageTerminal();
+        File file = new File("/home/sash/plot1.png");
+        try {
+            file.createNewFile();
+            png.processOutput(new FileInputStream(file));
+        } catch (FileNotFoundException ex) {
+            System.err.print(ex);
+        } catch (IOException ex) {
+            System.err.print(ex);
+        }
 
-//        Plot2DPanel plot = new Plot2DPanel();   // add a line plot to the PlotPanel
-//        plot.addLinePlot("Raw Data Samples Channel 0", x, converter.getChannel(0));   // put the PlotPanel in a JFrame, as a JPanel
-//        plot.addLinePlot("Raw Data Samples Channel 1", x, converter.getChannel(1));   // put the PlotPanel in a JFrame, as a JPanel
-//        frame.add(plot);
-//
-//        frame.setVisible(true);
-//        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        JavaPlot p = new JavaPlot();
+        p.setTerminal(png);
+        p.getAxis("x").setLabel("time");
+        p.getAxis("y").setLabel("price");
 
-        Plot2DPanel plot = new Plot2DPanel();
-        plot.addLinePlot("my plot", x, y);
-        JFrame frame = new JFrame("a plot panel");
-        frame.setContentPane(plot);
-        frame.setVisible(true);
-        frame.setSize(600, 600);
+        String resultMin = String.format("%.5f", min0).replace(',', '.');
+        String resultMax = String.format("%.5f", max0).replace(',', '.');
+        double boundMin = Double.parseDouble(resultMin) - (Double.parseDouble(resultMin) / 100);
+        double boundMax = Double.parseDouble(resultMax) + (Double.parseDouble(resultMax) / 100);
+        p.getAxis("y").setBoundaries(boundMin, boundMax);
+        p.getAxis("x").setBoundaries(-10, 110);
+
+        if (values0.length > 0) {
+            PlotStyle styleVal0 = new PlotStyle();
+            styleVal0.setStyle(Style.LINES);
+            styleVal0.setLineType(NamedPlotColor.RED);
+            DataSetPlot setVal0 = new DataSetPlot(values0);
+            setVal0.setPlotStyle(styleVal0);
+            setVal0.setTitle(legend);
+            p.addPlot(setVal0);
+        }
+
+//        if (values1.length > 0) {
+//            PlotStyle styleVal1 = new PlotStyle();
+//            styleVal1.setStyle(Style.LINES);
+//            styleVal1.setLineType(NamedPlotColor.BLUE);
+//            DataSetPlot setVal1 = new DataSetPlot(values1);
+//            setVal1.setPlotStyle(styleVal1);
+//            setVal1.setTitle(legend);
+//            p.addPlot(setVal1);
+//        }
+        p.plot();
+        try {
+            ImageIO.write(png.getImage(), "png", file);
+        } catch (IOException ex) {
+            System.err.print(ex);
+        }
     }
 }
