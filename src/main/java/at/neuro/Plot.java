5b62f657e0de69e18312b3b8ef7d3631b50024d3
sash <softester@yandex.ru>
2023-01-07 11:57:37 +0300

    Plot with saving to file

--- src/main/java/at/neuro/Plot.java
+++ src/main/java/at/neuro/Plot.java
@@ -6,8 +6,10 @@ import com.panayotis.gnuplot.style.NamedPlotColor;
 import com.panayotis.gnuplot.style.PlotStyle;
 import com.panayotis.gnuplot.style.Style;
 import com.panayotis.gnuplot.terminal.ImageTerminal;
+import org.math.plot.Plot2DPanel;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,6 +17,8 @@ import java.io.IOException;
 
 
 public class Plot {
+    public static double[] xx;
+    public static double[] yy;
     public static double[][] values0;
     public static double max0;
     public static double min0;
@@ -25,7 +29,9 @@ public class Plot {
 
     public static void plotInit() {
         ImageTerminal png = new ImageTerminal();
-        File file = new File("/home/sash/plot1.png");
+        String filePath = "/home/sash/plot_" + legend.replace(" ", "_")+ ".png";
+        File file = new File(filePath.toLowerCase());
+        System.out.println(filePath.toLowerCase());
         try {
             file.createNewFile();
             png.processOutput(new FileInputStream(file));
@@ -35,6 +41,13 @@ public class Plot {
             System.err.print(ex);
         }
 
+//        Plot2DPanel plot = new Plot2DPanel();
+//        plot.addLinePlot(legend, xx, yy);
+//        JFrame frame = new JFrame(legend);
+//        frame.setContentPane(plot);
+//        frame.setVisible(true);
+//        frame.setSize(600, 600);
+
         JavaPlot p = new JavaPlot();
         p.setTerminal(png);
         p.getAxis("x").setLabel("time");
@@ -42,15 +55,16 @@ public class Plot {
 
         String resultMin = String.format("%.5f", min0).replace(',', '.');
         String resultMax = String.format("%.5f", max0).replace(',', '.');
-        double boundMin = Double.parseDouble(resultMin) - (Double.parseDouble(resultMin) / 100);
-        double boundMax = Double.parseDouble(resultMax) + (Double.parseDouble(resultMax) / 100);
+        double boundMin = Double.parseDouble(resultMin) - (Double.parseDouble(resultMin) / 1000);
+        double boundMax = Double.parseDouble(resultMax) + (Double.parseDouble(resultMax) / 1000);
         p.getAxis("y").setBoundaries(boundMin, boundMax);
-        p.getAxis("x").setBoundaries(-10, 110);
+        p.getAxis("x").setBoundaries(-10, Klines.limit + 10);
 
         if (values0.length > 0) {
             PlotStyle styleVal0 = new PlotStyle();
             styleVal0.setStyle(Style.LINES);
             styleVal0.setLineType(NamedPlotColor.RED);
+            System.out.println(values0);
             DataSetPlot setVal0 = new DataSetPlot(values0);
             setVal0.setPlotStyle(styleVal0);
             setVal0.setTitle(legend);
